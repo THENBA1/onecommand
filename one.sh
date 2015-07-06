71c4229d8044a165ae87a583dca9025c1fd579c0
THENBA1 <thenba1@gmail.com>
2015-07-06 20:39:43 +0700

    Update one.sh
    v.2

--- one.sh
+++ one.sh
@@ -3,21 +3,21 @@ echo "==== Update ===="
 apt-get update
 
 echo "==== Upgrade ===="
-apt-get -y upgrade
+apt-get -yqq upgrade
 
 echo "==== Install Common Software ===="
-apt-get -y install software-properties-common
+apt-get -yqq install software-properties-common
 
 echo "==== Install Python Software ===="
-apt-get -y install python-software-properties
+apt-get -yqq install python-software-properties
 
 echo "==== Install Java ===="
-add-apt-repository -y ppa:webupd8team/java
+add-apt-repository -yqq ppa:webupd8team/java
 apt-get update
-apt-get -y install oracle-java8-installer
+apt-get -yqq install oracle-java8-installer
 
 echo "==== Install Flash Player ===="
-apt-get -y install flashplugin-installer
+apt-get -yqq install flashplugin-installer
 
 echo "==== Install XORG // LXDE-CORE // TIGHTVNCSERVER // NANO // FIREFOX // ZIP ===="
-apt-get -y install xorg lxde-core tightvncserver nano firefox zip
+apt-get -yqq install xorg lxde-core tightvncserver nano firefox zip
